c169e75966cf4ad5d7eb0a29ac1cdd074dbefb92
k <127846708+sarffff@users.noreply.github.com>
2026-07-30 18:33:13 +0800

    Update config.py

--- back-end/config.py
+++ back-end/config.py
@@ -2,11 +2,11 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    DATABASE_URL: str = "mysql+pymysql://root:2216@localhost:3306/ai_workspace"
+    DATABASE_URL: str = "你的数据库配置"
     PORT: int = 3000
-    LLM_API_KEY: str = "9aced57f3f7e492db501c7a9e97b00e4.DTb6DY07uatScrFF"
-    LLM_BASE_URL: str = "https://open.bigmodel.cn/api/paas/v4/"
-    LLM_MODEL: str = "glm-4.5-air"
+    LLM_API_KEY: str = "你的api"
+    LLM_BASE_URL: str = "大模型接口地址"
+    LLM_MODEL: str = "模型名称"
     REDIS_URL: str = ""
 
     class Config:
